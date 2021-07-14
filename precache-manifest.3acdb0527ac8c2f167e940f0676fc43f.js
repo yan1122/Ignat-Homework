63821fca55553939b68584a6c529811056f715e0
yan1122 <yan.tiger8@gmail.com>
2021-07-14 20:32:48 +0300

    Updates

---
+++ precache-manifest.3acdb0527ac8c2f167e940f0676fc43f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f77e13d7b3207bdef3332cc027f9d2",
+    "url": "/Ignat-Homework/index.html"
+  },
+  {
+    "revision": "2404e374935c2767c0bf",
+    "url": "/Ignat-Homework/static/css/main.bfa512a8.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/Ignat-Homework/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ignat-Homework/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2404e374935c2767c0bf",
+    "url": "/Ignat-Homework/static/js/main.135c9bfe.chunk.js"
+  },
+  {
+    "revision": "33bbed8833909467f63f",
+    "url": "/Ignat-Homework/static/js/runtime-main.7535c28a.js"
+  }
+]);
